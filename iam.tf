be08a632b800d443e715d37a7801ccc20b5ddf25
nitin-pandey-27 <nitin_pandey_27@yahoo.co.in>
2021-08-04 00:15:14 +0400

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,11 +1,11 @@
 resource "aws_iam_role" "eks_cluster" {
-  name = "eks-cluster"
+  name = "eksClusterRole"
   
   # resource name = eks_cluster
-  # IAM Role Name = eks-cluster
+  # IAM Role Name = eksClusterRole
   # Resource Detail - https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
   # Role Creation - https://docs.aws.amazon.com/eks/latest/userguide/service_IAM_role.html#create-service-role
-  # Role Details - https://docs.aws.amazon.com/eks/latest/userguide/service_IAM_role.html
+  # Role Policy - https://docs.aws.amazon.com/eks/latest/userguide/service_IAM_role.html
   
   assume_role_policy = <<EOF
 {
@@ -21,6 +21,12 @@ resource "aws_iam_role" "eks_cluster" {
   ]
 }
 EOF
+  
+  tags = {
+    sre_candidate = "${var.candidate_name}"
+    #Tag 
+  }
+  
 }
  
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
@@ -28,39 +34,58 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   roles = aws_iam_role.eks_cluster.name
   # Bind the role with the Policy "AmazonEKSClusterPolicy"
+  # Policy Binding - https://docs.aws.amazon.com/eks/latest/userguide/service_IAM_role.html
 }
 
-resource "aws_iam_role_policy_attachment" "AmazonEKSServiceRolePolicy" {
-  name = "AmazonEKSServiceRolePolicy"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServiceRolePolicy"
-  roles       = aws_iam_role.eks_cluster.name
-  # Bind the role with the Policy "AmazonEKSServiceRolePolicy"
-}
+#resource "aws_iam_role_policy_attachment" "AmazonEKSServiceRolePolicy" {
+#  name = "AmazonEKSServiceRolePolicy"
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServiceRolePolicy"
+#  roles       = aws_iam_role.eks_cluster.name
+#  # Bind the role with the Policy "AmazonEKSServiceRolePolicy"
+#}
 
 resource "aws_iam_role" "eks_nodes" {
-  name = "eks-nodes"
+  name = "eksNodeRole"
   
   # resource name = eks_nodes
-  # IAM Role Name = eks-nodes
+  # IAM Role Name = eksNodeRole
   # Resource Detail - https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
+  # Role Creation - https://docs.aws.amazon.com/eks/latest/userguide/create-node-role.html#create-worker-node-role
+  # Policy Attach - https://docs.aws.amazon.com/eks/latest/userguide/create-node-role.html#create-worker-node-role
   
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
       },
-    ]
-  })
+      "Action": "ec2.amazonaws.com"
+    }
+  ]
+}
+EOF
+  
+  tags = {
+    sre_candidate = "${var.candidate_name}"
+    #Tag 
+  }
+  
 }
  
-resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
-  name = "AmazonEKSClusterPolicy"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  roles = aws_iam_role.eks_cluster.name
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  name = "AmazonEKSWorkerNodePolicy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  roles = aws_iam_role.eks_nodes.name
   # Bind the role with the Policy "AmazonEKSClusterPolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  name = "AmazonEC2ContainerRegistryReadOnly"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  roles = aws_iam_role.eks_nodes.name
+  # Bind the role with the Policy "AmazonEKSClusterPolicy"
+}
+
