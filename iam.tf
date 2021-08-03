262390932b9482511c5a930b362bacf7e5bcad9c
nitin-pandey-27 <nitin_pandey_27@yahoo.co.in>
2021-08-03 23:45:18 +0400

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -19,13 +19,15 @@ resource "aws_iam_role" "eks_cluster" {
   })
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  name = "AmazonEKSClusterPolicy"
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster.name
+  roles = aws_iam_role.eks_cluster.name
   # Bind the role with the Policy "AmazonEKSClusterPolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSServiceRolePolicy" {
+  name = "AmazonEKSServiceRolePolicy"
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServiceRolePolicy"
-  role       = aws_iam_role.eks_cluster.name
+  roles       = aws_iam_role.eks_cluster.name
   # Bind the role with the Policy "AmazonEKSServiceRolePolicy"
 }
