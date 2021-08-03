688132c7c513e8a7ffea9de97ae799e2de2f3f81
nitin-pandey-27 <nitin_pandey_27@yahoo.co.in>
2021-08-03 23:25:47 +0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,11 +3,12 @@ terraform {
     aws = {
       source = "hashicorp/aws"
       version = "3.52.0"
+      # https://registry.terraform.io/providers/hashicorp/aws/latest - refer this link to get the version and how to use the provider
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  # Configuration options
+  # We will use "us-east-1" region for our testing
 }
