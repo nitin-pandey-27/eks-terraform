757dc71ab87a858c65fdcdd4fd32cee701f1f8b8
nitin-pandey-27 <nitin_pandey_27@yahoo.co.in>
2021-08-03 23:27:19 +0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,23 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
+  # For module detail. Please refer - https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
 
-  name = "eks-vpc"
+  name = "eks-vpc" 
   cidr = "10.0.0.0/16"
+  #Name of VPC and CODR
 
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  #AZ with 4 subnets, 2 subnet each for Private Network and Public Network
 
   enable_nat_gateway = false
   enable_vpn_gateway = false
+  #No need to enable any Gateway
 
   tags = {
     sre_candidate = "nitin-pandey"
+    #Tag 
   }
 }
