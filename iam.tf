53333eb8c026d6fa91d62d8384d8f6c7e4cccd5e
nitin-pandey-27 <nitin_pandey_27@yahoo.co.in>
2021-08-04 01:12:07 +0400

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -37,6 +37,13 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   # Policy Binding - https://docs.aws.amazon.com/eks/latest/userguide/service_IAM_role.html
 }
 
+resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster.name
+  # This policy is mentioned in Terraform 
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_cluster
+}
+
 
 resource "aws_iam_role" "eks_nodes" {
   name = "eksNodeRole"
@@ -83,4 +90,12 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   # Bind the role with the Policy "AmazonEC2ContainerRegistryReadOnly"
   # Refer - https://docs.aws.amazon.com/eks/latest/userguide/create-node-role.html#create-worker-node-role
 }
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role = aws_iam_role.eks_nodes.name
+  # This policy is mentioned in Terraform 
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eks_node_group
+}
+
 
