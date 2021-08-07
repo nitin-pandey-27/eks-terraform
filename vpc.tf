04b02249ffa8eeccf03208753840b80efde5e9ba
nitin-pandey-27 <nitin_pandey_27@yahoo.co.in>
2021-08-08 01:40:55 +0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,15 +7,15 @@ module "vpc" {
   cidr = "10.0.0.0/16"
   #Name of VPC and CODR
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-  #AZ with 4 subnets, 2 subnet each for Private Network and Public Network
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  #AZ with 6 subnets, 3 subnet each for Private Network and Public Network
 
-  enable_nat_gateway = false
-  enable_vpn_gateway = false
-  enable_dns_hostnames = false
-  #No need to enable any Gateway
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+  enable_dns_hostnames = true
+
   
   tags = {
     "kubernetes.io/cluster/${var.candidate_name}" = "shared"
